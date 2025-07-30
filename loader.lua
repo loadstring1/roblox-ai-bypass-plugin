b902c071621b5fa92b4d2f98650f66248b5a47b1
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2025-07-30 21:58:47 +0200

    Moved RunService check to loader.lua
    Please update your loader.lua script immediately if you use the old one

--- loader.lua
+++ loader.lua
@@ -1,2 +1,4 @@
 --put this in %localappdata%\Roblox\Plugins
+local runserv=game:GetService("RunService")
+if runserv:IsRunning() then return end
 loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/loadstring1/roblox-ai-bypass-plugin/refs/heads/main/aibypass.luau"))()
