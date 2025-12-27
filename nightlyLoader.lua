811227dae8ba05f645ab4b98ca891b43a92e40f8
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2025-12-28 00:59:56 +0100

    seperate loaders

--- nightlyLoader.lua
+++ nightlyLoader.lua
@@ -1,3 +1,5 @@
+--DO NOT USE THIS JUST DOWNLOAD loader.lua INSTEAD!!! NIGHTLY WILL HAVE FASTER UPDATES BUT THOSE UPDATES MIGHT BUG/GLITCH THE ENTIRE PLUGIN
+
 --put this in %localappdata%\Roblox\Plugins
 local runserv=game:GetService("RunService")
 if runserv:IsRunning() then return end
@@ -11,7 +13,7 @@ if typeof(bypassScript)=="table" and bypassScript.offlineMode
     return
 end
 
-local source=game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/loadstring1/roblox-ai-bypass-plugin/refs/heads/main/aibypass.luau")
+local source=game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/loadstring1/roblox-ai-bypass-plugin/refs/heads/nightly/aibypass.luau")
 plugin:SetSetting("bypassScript",{
     lastFetch=os.clock(),
     source=buffer.tostring(encoding:Base64Encode(buffer.fromstring(source)))
