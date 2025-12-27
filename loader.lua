d9fd6f41acb554769ab01a9ba2f54fc2dfbc72f8
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2025-12-28 00:56:11 +0100

    nightly v2.0
    - added offline mode (unlike competition now you can run my plugin without wifi)
    - faster loading times than my competiton
    - unwrapping modules should be faster now (so basically removing huge bypasses from modules)
    - caches container.rbxm for 24 hours and main bypass script cache is 10 minutes
    
    important !! please replace your loader.lua with the one from github repo for faster loading times

--- loader.lua
+++ loader.lua
@@ -1,4 +1,20 @@
 --put this in %localappdata%\Roblox\Plugins
 local runserv=game:GetService("RunService")
 if runserv:IsRunning() then return end
-loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/loadstring1/roblox-ai-bypass-plugin/refs/heads/main/aibypass.luau"))()
+
+local encoding=game:GetService("EncodingService")
+local bypassScript=plugin:GetSetting("bypassScript")
+
+if typeof(bypassScript)=="table" and bypassScript.offlineMode 
+    or typeof(bypassScript)=="table" and typeof(bypassScript.lastFetch)=="number" and os.clock()-bypassScript.lastFetch<600 then
+    loadstring(buffer.tostring(encoding:Base64Decode(buffer.fromstring(bypassScript.source))))()
+    return
+end
+
+local source=game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/loadstring1/roblox-ai-bypass-plugin/refs/heads/main/aibypass.luau")
+plugin:SetSetting("bypassScript",{
+    lastFetch=os.clock(),
+    source=buffer.tostring(encoding:Base64Encode(buffer.fromstring(source)))
+})
+
+loadstring(source)()
