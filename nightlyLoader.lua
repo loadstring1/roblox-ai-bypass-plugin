18e93113e89186f1f80530f070f95668a883ca19
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2025-12-28 01:56:48 +0100

    remove debug prints

--- nightlyLoader.lua
+++ nightlyLoader.lua
@@ -9,12 +9,9 @@ local bypassScript=plugin:GetSetting("bypassScript")
 
 if typeof(bypassScript)=="table" and bypassScript.offlineMode 
     or typeof(bypassScript)=="table" and typeof(bypassScript.lastFetch)=="number" and os.clock()-bypassScript.lastFetch<600 then
-    print("calling loadstring on cached script")
     loadstring(buffer.tostring(encoding:Base64Decode(buffer.fromstring(bypassScript.source))))()
     return
 end
-
-print("getting script from repo (sending http request)")
 
 local source=game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/loadstring1/roblox-ai-bypass-plugin/refs/heads/nightly/aibypass.luau")
 plugin:SetSetting("bypassScript",{
