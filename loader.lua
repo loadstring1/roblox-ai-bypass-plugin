3e50aab300573e29e5fc2532d758e9bc0a834d5c
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2025-12-28 11:13:36 +0100

    update loader for main settings

--- loader.lua
+++ loader.lua
@@ -3,18 +3,30 @@ local runserv=game:GetService("RunService")
 if runserv:IsRunning() then return end
 
 local encoding=game:GetService("EncodingService")
-local bypassScript=plugin:GetSetting("bypassScript")
+local pluginSettings=plugin:GetSetting("settings_aipluginbypass")
 
-if typeof(bypassScript)=="table" and bypassScript.offlineMode 
-    or typeof(bypassScript)=="table" and typeof(bypassScript.lastFetch)=="number" and os.clock()-bypassScript.lastFetch<600 then
+if typeof(pluginSettings)~="table" then
+    plugin:SetSetting("settings_aipluginbypass",{
+        data={},
+        offlineMode=false,
+        lastVersion="first_run",
+    })
+    pluginSettings=plugin:GetSetting("settings_aipluginbypass")
+end
+
+local bypassScript=pluginSettings.data.bypassScript
+
+if pluginSettings.offlineMode or typeof(bypassScript)=="table" and typeof(bypassScript.lastFetch)=="number" and os.clock()-bypassScript.lastFetch<600 then
     loadstring(buffer.tostring(encoding:Base64Decode(buffer.fromstring(bypassScript.source))))()
     return
 end
 
 local source=game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/loadstring1/roblox-ai-bypass-plugin/refs/heads/main/aibypass.luau")
-plugin:SetSetting("bypassScript",{
+
+pluginSettings.data.bypassScript={
     lastFetch=os.clock(),
     source=buffer.tostring(encoding:Base64Encode(buffer.fromstring(source)))
-})
+}
+plugin:SetSetting("settings_aipluginbypass",pluginSettings)
 
 loadstring(source)()
